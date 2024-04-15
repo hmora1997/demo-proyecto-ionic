25a915055f46d648a169e20cc97c7a92492986b0
Estudiappcl <estudiapp2022@gmail.com>
2024-04-15 12:55:50 -0400

    console para ver que datos se validaron

--- src/components/EppInsumos.jsx
+++ src/components/EppInsumos.jsx
@@ -17,21 +17,29 @@ import {
 } from "@ionic/react";
 import "./epp-insumos.css";
 import CustomModal from "./CustomModal";
-import Input from "./Input";
 import UsuarioActual from "./UsuarioActual";
 
 const EppInsumos = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [modalStage, setModalStage] = useState(0);
   const [cargos, setCargos] = useState([]);
   const [bodegas, setBodegas] = useState([]);
-  const [seleccionados, setSeleccionados] = useState([]);
-  const [bodegaSeleccionada, setBodegaSeleccionada] = useState("");
   const [cargoSeleccionado, setCargoSeleccionado] = useState("");
-  const [motivo, setMotivo] = useState("");
+  const [seleccionados, setSeleccionados] = useState([]);
   const [insumosSeleccionados, setInsumosSeleccionados] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [modalStage, setModalStage] = useState(0);
+  const [motivo, setMotivo] = useState("");
+  const [bodegaSeleccionada, setBodegaSeleccionada] = useState("");
 
   useEffect(() => {
+    const cargarCargos = async () => {
+      try {
+        const cargosObtenidos = await obtenerCargos();
+        setCargos(cargosObtenidos);
+      } catch (error) {
+        console.error("No se pudieron cargar los cargos:", error);
+      }
+    };
+
     const cargarBodegas = async () => {
       try {
         const bodegasObtenidas = await obtenerBodegas();
@@ -41,21 +49,8 @@ const EppInsumos = () => {
       }
     };
 
-    cargarBodegas();
-  }, []);
-
-  useEffect(() => {
-    const cargarCargos = async () => {
-      try {
-        const cargosObtenidos = await obtenerCargos();
-        setCargos(cargosObtenidos);
-      } catch (error) {
-        // Maneja el error, por ejemplo, mostrando un mensaje al usuario
-        console.error("No se pudieron cargar los cargos:", error);
-      }
-    };
-
     cargarCargos();
+    cargarBodegas();
   }, []);
 
   const onEliminar = (trabajadorId) => {
@@ -101,6 +96,13 @@ const EppInsumos = () => {
   };
 
   const handleAccept = () => {
+    console.log({
+      Cargo: cargos.find(cargo => cargo.CAR_ID === cargoSeleccionado)?.CAR_NOMBRE,
+      TrabajadoresSeleccionados: seleccionados,
+      InsumosSeleccionados: insumosSeleccionados,
+      Motivo: motivo,
+      Bodega: bodegas.find(bodega => bodega.BOD_ID === bodegaSeleccionada)?.BOD_NOMBRE,
+  });
     setModalStage(1);
     setTimeout(() => {
       setModalStage(2);
@@ -141,7 +143,7 @@ const EppInsumos = () => {
 
   return (
     <>
-      <CustomModal
+       <CustomModal
         isOpen={showModal}
         title={title}
         message={message}
@@ -154,14 +156,19 @@ const EppInsumos = () => {
         <div className="container-fluid px-5 mt-4">
           <h2 className=" mb-3">Entregar EPP o Insumos</h2>
           <IonLabel className="text-dark" position="stacked">
-            Cargo,Trabajadores e Insumos
+            Cargo, Trabajadores e Insumos
           </IonLabel>
           <IonItem className="input-item mb-2">
             <IonSelect
               value={cargoSeleccionado}
-              onIonChange={(e) => setCargoSeleccionado(e.detail.value)}
-              className="text-center"
-              placeholder="[Seleccionar Cargo]"
+              onIonChange={(e) => {
+                setCargoSeleccionado(e.detail.value);
+                setSeleccionados([]);
+                setInsumosSeleccionados([]);
+                setBodegaSeleccionada("");
+                setMotivo("");
+              }}
+              placeholder="Seleccionar Cargo"
             >
               {cargos.map((cargo) => (
                 <IonSelectOption key={cargo.CAR_ID} value={cargo.CAR_ID}>
@@ -170,77 +177,82 @@ const EppInsumos = () => {
               ))}
             </IonSelect>
           </IonItem>
-          <div className="container-fluid ">
-            <div className="row">
-              <div className="col-6 px-0 pe-1 pe-md-2">
-                <SelectorTrabajadores
-                  seleccionados={seleccionados}
-                  setSeleccionados={setSeleccionados}
-                />
-              </div>
-              <div className="col-6 px-0 ps-1 ps-md-2">
-                <SelectorInsumos
-                  insumosSeleccionados={insumosSeleccionados}
-                  setInsumosSeleccionados={setInsumosSeleccionados} // Pasas el setter para que SelectorInsumos pueda actualizar el estado global
-                />
+          {cargoSeleccionado && (
+            
+            <>
+               <div className="container-fluid">
+              <div className="row">
+                <div className="col-6 px-0 pe-1 pe-md-2">
+                  <SelectorTrabajadores
+                    seleccionados={seleccionados}
+                    setSeleccionados={setSeleccionados}
+                  />
+                </div>
+                <div className="col-6 px-0 ps-1 ps-md-2">
+                  {seleccionados.length > 0 && (
+                    <SelectorInsumos
+                      insumosSeleccionados={insumosSeleccionados}
+                      setInsumosSeleccionados={setInsumosSeleccionados}
+                    />
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-
-          <IonItem className="input-item mb-4">
-            <IonInput
-              type="text"
-              value={motivo}
-              clearInput
-              placeholder="Motivo de entrega"
-              onChange={(e) => setMotivo(e.detail.value)}
-              className=""
-            />
-          </IonItem>
-
-          <IonLabel className="text-dark" position="stacked">
-            Bodega
-          </IonLabel>
-          <IonItem className="input-item mb-4">
-            <IonSelect
-              className="text-center"
-              value={bodegaSeleccionada}
-              onIonChange={(e) => setBodegaSeleccionada(e.detail.value)}
-              placeholder="[Seleccionar Bodega]"
-            >
-              {bodegas.map((bodega) => (
-                <IonSelectOption key={bodega.BOD_ID} value={bodega.BOD_ID}>
-                  {bodega.BOD_NOMBRE}
-                </IonSelectOption>
-              ))}
-            </IonSelect>
-          </IonItem>
-          <IonLabel className="text-dark" position="stacked">
-            Resumen Trabajadores
-            <p className="fw-bold text-dark">Presione un trabajador para eliminar</p>
-          </IonLabel>
-          <TrabajadoresSeleccionados
-            seleccionados={seleccionados}
-            onEliminar={onEliminar}
-          />
-          <IonLabel className="text-dark" position="stacked">
-            Resumen EPP e Insumos
-            <p className="fw-bold text-dark">Presione un insumo para editar/eliminar</p>
-          </IonLabel>
-          <InsumosSeleccionados
-            seleccionados={insumosSeleccionados}
-            onEliminar={onEliminarInsumo} // Asumiendo que también tienes esta función
-            onEditar={onEditar}
-          />
-
+              <IonLabel className="text-dark" position="stacked">
+                Motivo de entrega
+              </IonLabel>
+              <IonItem className="input-item mb-4">
+                <IonInput
+                  type="text"
+                  value={motivo}
+                  onIonChange={(e) => setMotivo(e.detail.value)}
+                  placeholder="Ingrese el motivo"
+                />
+              </IonItem>
+              <IonLabel className="text-dark" position="stacked">
+                Bodega
+              </IonLabel>
+              <IonItem className="input-item mb-4">
+                <IonSelect
+                  value={bodegaSeleccionada}
+                  onIonChange={(e) => setBodegaSeleccionada(e.detail.value)}
+                  placeholder="Seleccionar Bodega"
+                >
+                  {bodegas.map((bodega) => (
+                    <IonSelectOption key={bodega.BOD_ID} value={bodega.BOD_ID}>
+                      {bodega.BOD_NOMBRE}
+                    </IonSelectOption>
+                  ))}
+                </IonSelect>
+              </IonItem>
+              <TrabajadoresSeleccionados
+                seleccionados={seleccionados}
+                onEliminar={(trabajadorId) => {
+                  const nuevosSeleccionados = seleccionados.filter(
+                    trabajador => trabajador.TRA_ID !== trabajadorId
+                  );
+                  setSeleccionados(nuevosSeleccionados);
+                }}
+              />
+              {insumosSeleccionados.length > 0 && (
+                        <InsumosSeleccionados
+                        seleccionados={insumosSeleccionados}
+                        onEliminar={onEliminarInsumo} // Asumiendo que también tienes esta función
+                        onEditar={onEditar}
+                      />
+              )}
+            </>
+          )}
           <IonButton
-            className="mx-0 mt-4 mb-3 fw-bold button-blue"
             expand="block"
             onClick={() => setShowModal(true)}
+            disabled={!bodegaSeleccionada || seleccionados.length === 0 || insumosSeleccionados.length === 0}
+            className="mx-0 mt-4 mb-3 fw-bold button-blue"
           >
             Validar y entregar
           </IonButton>
         </div>
+       
       </IonContent>
     </>
   );
