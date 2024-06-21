0508d796497cc72991a41e39ac04c511faecbad6
Héctor Mora <hmora1997gmail.com>
2024-06-21 18:04:23 -0400

    se agrego fecha al insert de insumo

--- src/services/insert.js
+++ src/services/insert.js
@@ -21,10 +21,13 @@ export const enviarSolicitudes = async (
   const year = currentDate.getFullYear();
   const month = String(currentDate.getMonth() + 1).padStart(2, '0');
   const day = String(currentDate.getDate()).padStart(2, '0');
+  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
 
   trabajadores.forEach((trabajador, indexT) => {
     insumosSeleccionados.forEach((insumo, indexI) => {
-      const formattedDate = `${year}-${month}-${day}`;
       const firmaTrabajador = firmas[trabajador.tra_id] || "";
 
       console.log('Enviando firma para trabajador:', trabajador.tra_id, 'Firma:', firmaTrabajador);
